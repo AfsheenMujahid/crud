05b8d0aba7a30edb2ae55e7850ce88400f5efd3c
Afsheen Mujahid <mujahidhussain1632000@gmail.com>
2024-03-06 13:34:17 +0500

    commit

--- server.mjs
+++ server.mjs
@@ -206,7 +206,7 @@ app.use((req, res) => {
   res.status(404).send("not found");
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
