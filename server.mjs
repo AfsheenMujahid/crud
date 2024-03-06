db03c7cb31f4898ae0f250709bd512da7253a306
Afsheen Mujahid <mujahidhussain1632000@gmail.com>
2024-03-06 13:26:39 +0500

    update

--- server.mjs
+++ server.mjs
@@ -7,7 +7,8 @@ import cors from 'cors'
 //import './config/index.mjs'
 import path from "path";
 const __dirname = path.resolve();
-const mongodbURI = `mongodb+srv://AfsheenMujahid:<password>@cluster0.x9vgkks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}
+@${process.env.CLUSTER_NAME}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(mongodbURI);
 const database = client.db('CRUD');
